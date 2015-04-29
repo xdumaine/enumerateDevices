24875e8936ec0120ca4256ea72c2ed37eedfec00
Xander Dumaine <xander.dumaine@inin.com>
2015-04-29 10:04:01 -0400

    simplify server.js and add https option

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-var connect = require('connect'),
-    serveStatic = require('serve-static');
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var pem = require('pem');
 
-connect().use(serveStatic(__dirname)).listen('8880');
+pem.createCertificate({ days:1, selfSigned:true }, function(err, keys) {
+    var options = {
+      key: keys.serviceKey,
+      cert: keys.certificate
+    };
 
-console.log('Server running at http://127.0.0.1:8880/');
+    var app = express();
+
+    app.use(express.static('.'));
+
+    // Create an HTTP service.
+    http.createServer(app).listen(8880);
+    // Create an HTTPS service identical to the HTTP service.
+    https.createServer(options, app).listen(8881);
+
+    console.log('serving on http://localhost:8880 and https://localhost:8881');
+});
